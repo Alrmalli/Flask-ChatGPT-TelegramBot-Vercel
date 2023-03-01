d7e4c9993fa32bbe4f41fe3572f645dca5628aea
Alrmalli <64164976+Alrmalli@users.noreply.github.com>
2023-03-01 18:56:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import logging
-import os
-import telegram
+
+import telegram, os
 from flask import Flask, request
 from telegram.ext import Dispatcher, MessageHandler, Filters
+
+
+
+#################
 import openai
+	
+openai.api_key = os.getenv("OPENAI_API_KEY") 
 
-# Set OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Set the language of the chat bot
-chat_language = os.getenv("INIT_LANGUAGE", default="en")
-
-# Set the message limit and language table
-MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default=20))
+chat_language = os.getenv("INIT_LANGUAGE", default = "en") #amend here to change your preset language
+	
+MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
 LANGUAGE_TABLE = {
-    "zh": "Hello!",
-    "en": "Hello!",
-    "jp": "Hello!"
-}
+	  "zh": "哈囉！",
+	  "en": "Hello!",
+      "jp": "こんにちは"
+	}
 
-# Class to handle prompts
+
 class Prompts:
     def __init__(self):
         self.msg_list = []
@@ -36,15 +38,14 @@ class Prompts:
     def generate_prompt(self):
         return '\n'.join(self.msg_list)	
 	
-# Class for the ChatGPT model  
 class ChatGPT:  
     def __init__(self):
         self.prompt = Prompts()
-        self.model = os.getenv("OPENAI_MODEL", default="text-davinci-003")
-        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0))
-        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default=0))
-        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default=0.6))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=240))
+        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
+        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
+        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240)) #You can change here to decide the characer number AI gave you.
 	
     def get_response(self):
         response = openai.Completion.create(
@@ -56,10 +57,10 @@ class ChatGPT:
 	            max_tokens=self.max_tokens
                 )
         
-        print("AI response:")        
+        print("Ai prompt(The direct answer that AI gave you)：")        
         print(response['choices'][0]['text'].strip())
 
-        print("Original response data from AI:")      
+        print("AI original prompt(The original answer that AI gave you)：")      
         print(response)
         
         return response['choices'][0]['text'].strip()
@@ -67,52 +68,63 @@ class ChatGPT:
     def add_msg(self, text):
         self.prompt.add_msg(text)
 
-# Set Telegram bot token
+
+
+
+
+
+#####################
+
 telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+
+
+
+# Load data from config.ini file
+#config = configparser.ConfigParser()
+#config.read('config.ini')
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Initialize Flask app
+# Initial Flask app
 app = Flask(__name__)
 
-# Initialize bot with Telegram access token
+# Initial bot by Telegram access token
 bot = telegram.Bot(token=telegram_bot_token)
 
-# Function to handle incoming messages
-def reply_handler(bot, update):
-    # Create instance of ChatGPT
-    chatgpt = ChatGPT()        
-    
-    # Add the user's message to the prompt
-    chatgpt.prompt.add_msg(update.message.text)
-    
-    # Get the AI's response
-    ai_reply_response = chatgpt.get_response()
-    
-    # Reply to the user with the AI's response
-    update.message.reply_text(ai_reply_response)
 
 
-# Route to handle incoming messages
 @app.route('/callback', methods=['POST'])
 def webhook_handler():
+    """Set route /hook with POST method will trigger this method."""
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-        # Update dispatcher to process this message
+        # Update dispatcher process that handler to process this message
         dispatcher.process_update(update)
-        
     return 'ok'
 
-# Initialize dispatcher for the bot
+
+def reply_handler(bot, update):
+    """Reply message."""
+    #text = update.message.text
+    #update.message.reply_text(text)
+    chatgpt = ChatGPT()        
+    
+    chatgpt.prompt.add_msg(update.message.text) #人類的問題 the question humans asked
+    ai_reply_response = chatgpt.get_response() #ChatGPT產生的回答 the answers that ChatGPT gave
+    
+    update.message.reply_text(ai_reply_response) #用AI的文字回傳 reply the text that AI made
+
+# New a dispatcher for bot
 dispatcher = Dispatcher(bot, None)
 
-# Add handler to process text messages
+# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
+# message.
 dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
 
-#Start the bot
-if __name__ == 'main':
-    app.run()
+if __name__ == "__main__":
+    # Running server
+    app.run(debug=True)
